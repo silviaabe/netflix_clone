d8e422cc1c1d97bdc488aef3c48581de4a23b097
Silvia Abe <silviaabebuzatto@gmail.com>
2025-01-30 13:27:05 -0300

    finalizando  o site

--- src/Row.js
+++ src/Row.js
@@ -23,7 +23,6 @@ function Row({ title, fetchUrl, isLargeRow }) {
     height: "390",
     width: "100%",
     playerVars: {
-      // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
     },
   };
